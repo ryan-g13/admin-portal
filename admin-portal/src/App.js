8716c2fbde3b3f4c88a2261306072c3362abc07c
ryan-g13 <ryan.groesch@gmail.com>
2023-09-20 16:50:51 -0700

    Form with validation via yup is completed. Button for submission is console.log

--- admin-portal/src/App.js
+++ admin-portal/src/App.js
@@ -9,7 +9,7 @@ import Invoices from './scenes/Invoices';
 // import Calendar from './scenes/Calendar';
 import Contacts from './scenes/Contacts';
 // import Bar from './scenes/Bar';
-// import Form from './scenes/Form';
+import Form from './scenes/Form';
 // import Line from './scenes/Line';
 // import Pie from './scenes/Pie';
 // import FAQ from './scenes/FAQ';
@@ -32,7 +32,7 @@ function App() {
               <Route path='/contacts' element={<Contacts/>} />
               <Route path='/invoices' element={<Invoices/>} />
               {/* <Route path='/bar' element={<Bar/>} /> */}
-              {/* <Route path='/form' element={<Form/>} /> */}
+              <Route path='/form' element={<Form/>} />
               {/* <Route path='/line' element={<Line/>} /> */}
               {/* <Route path='/pie' element={<Pie/>} /> */}
               {/* <Route path='/faq' element={<FAQ/>} /> */}
