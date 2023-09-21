37b1a7b948005b41ea8fcf3ce268abd3b787181e
ryan-g13 <ryan.groesch@gmail.com>
2023-09-20 17:37:51 -0700

    * new packages, Calendar page is updated/ showing.

--- admin-portal/src/App.js
+++ admin-portal/src/App.js
@@ -6,7 +6,7 @@ import Sidebar from './scenes/global/Sidebar';
 import Dashboard from './scenes/dashboard';
 import Team from './scenes/Team';
 import Invoices from './scenes/Invoices';
-// import Calendar from './scenes/Calendar';
+import Calendar from './scenes/Calendar';
 import Contacts from './scenes/Contacts';
 // import Bar from './scenes/Bar';
 import Form from './scenes/Form';
@@ -28,16 +28,16 @@ function App() {
             <Header />
             <Routes>
               <Route path='/' element={<Dashboard/>} />
-              <Route path='/team' element={<Team/>} />
+              {/* <Route path='/bar' element={<Bar/>} /> */}
+              <Route path='/calendar' element={<Calendar/>} />
               <Route path='/contacts' element={<Contacts/>} />
               <Route path='/invoices' element={<Invoices/>} />
-              {/* <Route path='/bar' element={<Bar/>} /> */}
+              {/* <Route path='/faq' element={<FAQ/>} /> */}
               <Route path='/form' element={<Form/>} />
+              {/* <Route path='/geography' element={<Geography/>} /> */}
               {/* <Route path='/line' element={<Line/>} /> */}
               {/* <Route path='/pie' element={<Pie/>} /> */}
-              {/* <Route path='/faq' element={<FAQ/>} /> */}
-              {/* <Route path='/geography' element={<Geography/>} /> */}
-              {/* <Route path='/calendar' element={<Calendar/>} /> */}
+              <Route path='/team' element={<Team/>} />
             </Routes>
           </main>
         </div>
